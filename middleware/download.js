5d92e99c44d278d73bd70b260fdffebf5ed206b8
YouTwitFace <YouTwitFaceTG@gmail.com>
2019-01-01 17:17:52 -0500

    Add middleware to download files

---
+++ middleware/download.js
@@ -0,0 +1,28 @@
+`use strict`;
+
+const request = require(`request-promise`);
+
+module.exports = (bot) => {
+    const downloadUrl = `https://api.telegram.org/file/bot${bot.token}`;
+
+    bot.context.downloadFile = async function () {
+        const documentID = this.message.document.file_id;
+        const file = await bot.telegram.getFile(documentID);
+
+        return await request({
+            uri: `${downloadUrl}/${file.file_path}`,
+            encoding: null,
+        });
+    };
+
+    bot.context.downloadPhoto = async function () {
+        const photos = this.message.photo;
+        const photo = photos.pop().file_id;
+        const file = await bot.telegram.getFile(photo);
+
+        return await request({
+            uri: `${downloadUrl}/${file.file_path}`,
+            encoding: null,
+        });
+    };
+};
--- middleware/index.js
+++ middleware/index.js
@@ -1,6 +1,6 @@
 `use strict`;
 
-const modules = [`action`];
+const modules = [`action`, `download`];
 
 module.exports = (bot) =>
     modules.forEach(middleware =>
