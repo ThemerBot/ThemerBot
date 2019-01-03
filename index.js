5f1ce46459e9d002bc74e9844297c2b9e3b526ca
YouTwitFace <YouTwitFaceTG@gmail.com>
2019-01-02 22:02:46 -0500

    Add somewhere to store user themes

--- index.js
+++ index.js
@@ -8,9 +8,14 @@ const {
 } = process.env;
 
 const Telegraf = require(`telegraf`);
+const session = require(`telegraf/session`);
 const bot = new Telegraf(TOKEN, {
     username: USERNAME,
 });
+
+bot.use(session({
+    property: `theme`,
+}));
 
 require(`./middleware`)(bot);
 require(`./handlers`)(bot);
