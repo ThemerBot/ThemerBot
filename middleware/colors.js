19a53ef40a57f2f12e32b42c4cc3dd96978a6b08
YouTwitFace <YouTwitFaceTG@gmail.com>
2019-01-03 23:00:59 -0500

    Add a function to make the colors preview (not theme preview)

--- middleware/colors.js
+++ middleware/colors.js
@@ -1,8 +1,31 @@
+const fs = require(`promise-fs`);
+const { DOMParser, XMLSerializer } = require(`xmldom`);
+const { serializeToString: serialize } = new XMLSerializer();
+const sharp = require(`sharp`);
 const getColors = require(`get-image-colors`);
 
-module.exports = (bot) => {
+module.exports = bot => {
     bot.context.getImageColors = async (buffer, type) => {
         const colors = await getColors(buffer, type || `image/jpeg`);
         return colors.map(color => color.hex());
+    };
+
+    bot.context.makeColorsPreview = async colors => {
+        const svgFile = await fs.readFile(`./colors.svg`, `utf8`);
+        const svg = new DOMParser().parseFromString(svgFile);
+
+        let rects = svg.getElementsByTagName(`rect`);
+        rects = Array.prototype.slice.call(rects, 0, 5);
+        rects.forEach((rect, index) => rect.setAttribute(`fill`, colors[index]));
+
+        const defaultColors = svg.getElementsByTagName(`path`);
+        defaultColors[0].setAttribute(`fill`, colors[4]);
+        defaultColors[1].setAttribute(`fill`, colors[3]);
+        defaultColors[2].setAttribute(`fill`, colors[0]);
+
+        const svgBuffer = Buffer.from(serialize(svg), `binary`);
+        const image = await sharp(svgBuffer).png().toBuffer();
+
+        return image;
     };
 };
