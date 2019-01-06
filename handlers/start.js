a72c1c86460c66cbb161347c2cf3d4225b272623
YouTwitFace <YouTwitFaceTG@gmail.com>
2019-01-05 20:37:23 -0500

    Only allow /start in private

--- handlers/start.js
+++ handlers/start.js
@@ -1,10 +1,12 @@
 module.exports = bot => {
     bot.command(`start`, async ctx => {
-        const typing = ctx.action();
-        const { first_name, last_name } = ctx.from;
-        const name = `${first_name} ${last_name || ``}`.trim();
+        if (ctx.chat.type === `private`) {
+            const typing = ctx.action();
+            const { first_name, last_name } = ctx.from;
+            const name = `${first_name} ${last_name || ``}`.trim();
 
-        await ctx.reply(ctx.i18n(`start`, { name }));
-        typing.stop();
+            await ctx.reply(ctx.i18n(`start`, { name }));
+            typing.stop();
+        }
     });
 };
