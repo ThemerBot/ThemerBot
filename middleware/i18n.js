dbf694015bd55e3bbd54006523d9c2337204f3e3
YouTwitFace <YouTwitFaceTG@gmail.com>
2019-01-05 23:37:04 -0500

    Put i18n languages in .env

--- middleware/i18n.js
+++ middleware/i18n.js
@@ -1,5 +1,6 @@
 const newI18n = require(`new-i18n`);
-const i18n = newI18n(`${__dirname}/../i18n`, [`en`, `hi`, `es`]);
+const languages = process.env.I18N_LANGUAGES.split(`,`);
+const i18n = newI18n(`${__dirname}/../i18n`, languages);
 
 module.exports = bot => {
     bot.context.i18n = function (keyword, variables) {
