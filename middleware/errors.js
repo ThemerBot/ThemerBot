b955e78884c33f2c46e43673fcceac506a0adcbd
YouTwitFace <YouTwitFaceTG@gmail.com>
2019-01-05 23:37:45 -0500

    Add error handler and log channel

---
+++ middleware/errors.js
@@ -0,0 +1,15 @@
+/* eslint-disable quotes, no-console */
+
+module.exports = bot => {
+    bot.catch(error => {
+        if (process.env.LOG_CHANNEL) {
+            bot.telegram.sendMessage(
+                process.env.LOG_CHANNEL,
+                '```\n' + error.toString() + '```',
+                { parse_mode: `markdown` }
+            );
+        } else {
+            console.error((error.stack || error.toString()).replace(/^/gm, `  `));
+        }
+    });
+};
--- middleware/index.js
+++ middleware/index.js
@@ -1,4 +1,5 @@
 const modules = [
+    `errors`,
     `action`,
     `download`,
     `keyboard`,
