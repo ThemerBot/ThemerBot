4a9a7689a332caa5fa4835f58cfa476a3c876e54
YouTwitFace <contact@lungers.com>
2019-11-14 22:34:37 -0500

    Remove unfinished themes from memory after 5 minutes of inactivity

--- middleware/storage.js
+++ middleware/storage.js
@@ -2,18 +2,47 @@ const store = new Map();
 
 const getKey = ctx => `${ctx.chat.id}:${ctx.from.id}`;
 
+const removeTheme = (key, messageId) => {
+    const usersThemes = store.get(key) || {};
+
+    if (usersThemes[messageId]) {
+        delete usersThemes[messageId];
+    }
+
+    if (Object.keys(usersThemes).length === 0) {
+        store.delete(key);
+    } else {
+        store.set(key, usersThemes);
+    }
+};
+
 module.exports = bot => {
     bot.context.saveTheme = function(messageId, theme) {
         const key = getKey(this);
         const usersThemes = store.get(key) || {};
 
+        if (usersThemes[messageId]) {
+            clearTimeout(usersThemes[messageId].timeout);
+        }
+
         if (theme === null) {
             delete usersThemes[messageId];
         } else {
-            usersThemes[messageId] = { ...theme };
+            const timeout = setTimeout(
+                removeTheme,
+                1000 * 60 * 5, // 5 minutes
+                key,
+                messageId
+            );
+
+            usersThemes[messageId] = { ...theme, timeout };
         }
 
-        store.set(key, usersThemes);
+        if (Object.keys(usersThemes).length === 0) {
+            store.delete(key);
+        } else {
+            store.set(key, usersThemes);
+        }
     };
 
     bot.context.getTheme = function(messageId) {
