088391efe38146f4b508d46439f5cbf6e80214e6
YouTwitFace <contact@lungers.com>
2019-11-17 22:11:07 -0500

    Get i18n files from POEditor

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # ThemerBot
+
+## Instructions
+
+- Copy `.env.example` to `.env`
+  - Get your POEditor API token from https://poeditor.com/account/api
+- Run `npm start`
+
+## Translations
+
+> Translation files are downloaded from [POEditor](https://poeditor.com) when you run the bot for the first time. If you would like to redownload them, you can either delete the `i18n` directory and restart the bot or run `npm run i18n`.
+
+## Translators
+
+- English
+  - [YouTwitFace](https://github.com/YouTwitFace)
+  - [Hosein Khansari](https://github.com/hkh12)
+- Spanish
+  - [Erickson](https://t.me/ericksonx)
+- Farsi
+  - [Shaygan Shokrollahi](https://github.com/sh-sh-dev)
+  - [Hosein Khansari](https://github.com/hkh12)
+- Hindi
+  - [Lulzx](https://t.me/Lulzx)
+- Italian
+  - [Red](https://t.me/redhz)
---
+++ scripts/download-translations.js
@@ -0,0 +1,66 @@
+/* eslint-disable no-console */
+
+const fs = require(`fs`);
+const path = require(`path`);
+const request = require(`request-promise`);
+
+const getResponse = async (apiPath, data) => {
+    const { PO_API_TOKEN, PO_PROJECT_ID = 235195 } = process.env;
+
+    const { response, result } = await request({
+        method: `POST`,
+        uri: `https://api.poeditor.com/v2${apiPath}`,
+        json: true,
+        formData: {
+            api_token: PO_API_TOKEN,
+            id: PO_PROJECT_ID,
+            ...data,
+        },
+    });
+
+    if (response.code !== `200`) {
+        console.log(response.message);
+        process.exit();
+    }
+
+    return result;
+};
+
+const main = async () => {
+    const { languages } = await getResponse(`/languages/list`);
+    const dirPath = path.join(__dirname, `../i18n`);
+
+    try {
+        fs.mkdirSync(dirPath);
+    } catch (_) {
+        // Ignore errors
+    }
+
+    return new Promise(async resovle => {
+        for (const index in languages) {
+            const { name, code } = languages[index];
+            const filePath = path.join(dirPath, `${code}.json`);
+            const { url } = await getResponse(`/projects/export`, {
+                language: code,
+                type: `key_value_json`,
+            });
+
+            request(url)
+                .pipe(fs.createWriteStream(filePath))
+                .on(`close`, () => {
+                    console.log(`Saved ${name}`);
+
+                    if (Number(index) === languages.length - 1) {
+                        resovle();
+                    }
+                });
+        }
+    });
+};
+
+if (require.main === module) {
+    require(`dotenv`).config();
+    main();
+} else {
+    module.exports = main;
+}
