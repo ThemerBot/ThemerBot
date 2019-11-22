96b768fa01e57b3bb7fc628fda4d13052f1eacb0
YouTwitFace <contact@lungers.com>
2019-11-21 22:26:39 -0500

    Remove unnecessary typing in /start

--- handlers/help.js
+++ handlers/help.js
@@ -1,12 +1,10 @@
 module.exports = bot => {
     bot.command(`start`, async ctx => {
         if (ctx.chat.type === `private`) {
-            const typing = ctx.action();
             const { first_name, last_name } = ctx.from;
             const name = `${first_name} ${last_name || ``}`.trim();
 
             await ctx.reply(ctx.i18n(`start`, { name }));
-            typing.stop();
         }
     });
 
