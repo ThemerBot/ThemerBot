254f809f092481ad36d934e20d3a025898b9e681
Andrew Lane <contact@lungers.com>
2020-07-08 02:23:42 -0400

    Fix i18n

--- middleware/i18n.js
+++ middleware/i18n.js
@@ -1,19 +1,19 @@
 const fs = require(`fs`);
 const path = require(`path`);
-const newI18n = require(`new-i18n`);
+const I18n = require(`new-i18n`).default;
 
 const i18nDir = path.join(__dirname, `../i18n`);
 const languages = fs
     .readdirSync(i18nDir)
     .filter(file => file.endsWith(`.json`))
     .map(file => file.slice(0, -5));
-const i18n = newI18n(i18nDir, languages, `en`);
+const i18n = new I18n(i18nDir, languages, `en`);
 
 module.exports = bot => {
     bot.context.i18n = function(keyword, variables) {
         let lang = this.from.language_code;
         lang = i18n.languages.includes(lang) ? lang : `en`;
 
-        return i18n(lang, keyword, variables);
+        return i18n.translate(lang, keyword, variables);
     };
 };
