3f2cfd42f94442e694025e4d340d7c2eab7cfed4
Andrew Lane <contact@lungers.com>
2020-10-05 23:07:24 -0400

    Always log errors to console even if Sentry is enabled

--- middleware/errors.js
+++ middleware/errors.js
@@ -2,12 +2,7 @@ const Sentry = require(`@sentry/node`);
 
 module.exports = bot => {
     bot.catch(error => {
-        if (process.env.SENTRY_DSN) {
-            Sentry.captureException(error);
-        } else {
-            console.error(
-                (error.stack || error.toString()).replace(/^/gm, `  `),
-            );
-        }
+        console.error((error.stack || error.toString()).replace(/^/gm, `  `));
+        Sentry.captureException(error);
     });
 };
