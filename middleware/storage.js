bdf8d83418048d2eb8310d07e60eb6c01501f4c2
Emiliano Córdoba <31087818+emicba@users.noreply.github.com>
2021-01-13 00:43:34 -0300

    Add Docker files. Closes #20 :sparkles: (#21)

--- middleware/storage.js
+++ middleware/storage.js
@@ -7,7 +7,10 @@ const Sentry = require(`@sentry/node`);
 const redis = require(`redis`);
 const debug = require(`debug`)(`themerbot:middleware:storage`);
 
-const client = redis.createClient();
+const client = redis.createClient({
+    host: env.REDIS_HOST,
+    port: env.REDIS_PORT,
+});
 
 if (!env.LOCAL_API_ROOT) {
     mkdirp.sync(env.IMAGES_PATH);
